a9005c7e67a2befadafc480c9b7cfd16ecfe50a5
dwahroianmai <105296071+dwahroianmai@users.noreply.github.com>
2022-12-23 00:37:21 +0100

    Write computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+/*  1. getComputerChoiceRandomly returns rock,
+    paper or scissors.
+    2. playRound is a function that plays a single
+    round of the game, parameters case-insensitive. 
+    3. function called game, 5 rounds, keeps score
+    and reports a winner. */
+
+function getComputerChoiceRandomly() {
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    if (computerChoice === 1) {
+        return "rock";
+    } else if (computerChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
